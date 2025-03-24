b04482ece0be90cbdf20958d5754fc80da4df036
Eramood <122992858+Eramood@users.noreply.github.com>
2025-03-24 20:43:09 +0100

    complete the auth features + login + registration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
   import { Header } from "$components";
-    import "./../app.css";
-    let{children} = $props();
+  import "./../app.css";
+  let { data, children } = $props();
+
+  import { invalidate } from "$app/navigation";
+
+  let { session, supabase, user } = $derived(data);
+
+  $effect(()=>{
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth");
+      }
+    });
+    return () => data.subscription.unsubscribe();
+  });
+
+  
 </script>
+
 <Header />
 {@render children()};
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -4,32 +4,25 @@ interface RegisterReturnObject {
   success: boolean;
   email:string;
   password:string;
-  passwordConfirmation:string;
-  name:string;
+  passwordConfirmation?:never;
+  name?:never;
   errors: string[];
 }
 export const actions = {
   default: async ({ request, locals:{supabase} }) => {
     const formData = await request.formData();
-    const name = formData.get("name") as string;
+    
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-    const passwordConfirmation = formData.get("passwordConfirmation") as string;
+    
 
     const returnObject: RegisterReturnObject = {
       success: true,
-      name,
       email,
       password,
-      passwordConfirmation,
       errors: [],
     };
 
-    if (name.length < 3) {
-      returnObject.errors.push(
-        "The name is too short, must be lengt 3 characters"
-      );
-    }
     if (!email.length) {
       returnObject.errors.push("email required.");
     }
@@ -37,24 +30,16 @@ export const actions = {
       returnObject.errors.push("password required");
     }
 
-    if (password !== passwordConfirmation) {
-      returnObject.errors.push(
-        "the password and confirmation password do not match"
-      );
-    }
-
     if (returnObject.errors.length) {
       returnObject.success = false;
       return returnObject;
     }
    
-    const {data,error} =await supabase.auth.signUp({
+    const {data,error} =await supabase.auth.signInWithPassword({
         email,
         password,
      });
      if(error || !data.user){
-        console.log("there as an errr");
-        console.log(error);
         returnObject.success=false;
         return fail(400, returnObject as any)
      }
