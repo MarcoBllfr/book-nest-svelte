5735767b05a0567fffca41b185e58cc28d1d7ac5
marcobllfr <122992858+Eramood@users.noreply.github.com>
2025-03-28 15:57:53 +0100

    create the fetch + retrive data from books table

--- src/lib/components/state/user-state.svelte.ts
+++ src/lib/components/state/user-state.svelte.ts
@@ -1,41 +1,72 @@
 import { goto } from "$app/navigation";
+import type { Database } from "$lib/types/database.types";
 import type { Session, SupabaseClient, User } from "@supabase/supabase-js";
 import { getContext, setContext } from "svelte";
 
-interface UserStateProps{
-    session:Session | null;
-    supabase:SupabaseClient | null;
-    user:User | null;
+interface UserStateProps {
+  session: Session | null;
+  supabase: SupabaseClient | null;
+  user: User | null;
 }
+interface Book {
+  author: string | null;
+  cover_image: string | null;
+  created_at: string;
+  description: string | null;
+  finished_reading_on: string | null;
+  genre: string | null;
+  id: number;
+  rating: number | null;
+  started_reading_on: string | null;
+  title: string | null;
+  user_id: string;
+}
+export class UserState {
+  session = $state<Session | null>(null);
+  supabase = $state<SupabaseClient<Database> | null>(null);
+  user = $state<User | null>(null);
+  allBooks = $state<Book[]>([]);
 
-export class UserState{
-    session=$state<Session | null>(null);
-    supabase=$state<SupabaseClient | null>(null);
-    user=$state<User | null>(null);
+  constructor(data: UserStateProps) {
+    this.updateState(data);
+  }
 
-    constructor(data:UserStateProps){
-        this.updateState(data);   
+  updateState(data: UserStateProps) {
+    this.session = data.session;
+    this.supabase = data.supabase;
+    this.user = data.user;
+    this.fetchUserData();
+  }
+
+  async fetchUserData() {
+    if (!this.user || !this.supabase) {
+      return;
     }
+    const { data, erorr } = await this.supabase
+      .from("books")
+      .select("*")
+      .eq("user_id", this.user.id);
 
-    updateState(data: UserStateProps){
-        this.session=data.session;
-        this.supabase=data.supabase;
-        this.user=data.user;
+    if (erorr) {
+      console.log("Error to fetch user books");
+      console.log(erorr);
+      return
     }
+    this.allBooks = data;
+  }
 
-   async logout(){
-        await this.supabase?.auth.signOut();
-        goto("/login");
-    }
-    
+  async logout() {
+    await this.supabase?.auth.signOut();
+    goto("/login");
+  }
 }
 
 const USER_STATE_KEY = Symbol("USER_STATE");
 
-export function setUserState(data: UserStateProps){
-    return setContext(USER_STATE_KEY, new UserState(data));
+export function setUserState(data: UserStateProps) {
+  return setContext(USER_STATE_KEY, new UserState(data));
 }
 
-export function getUserState(){
-    return getContext<ReturnType<typeof setUserState>>(USER_STATE_KEY); 
+export function getUserState() {
+  return getContext<ReturnType<typeof setUserState>>(USER_STATE_KEY);
 }
---
+++ src/routes/private/dashboard/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { getUserState } from "$components/state/user-state.svelte";
+
+    let userContext= getUserState();
+    let {allBooks} = $derived(userContext);
+   
+</script>
+
+{#each allBooks  as books }
+    <li>{books.title}</li>
+{/each}
